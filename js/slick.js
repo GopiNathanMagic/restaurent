0cec06c37bda089e263815ec6b606a778681fd30
Gopinathan S <151734545+GopiNathanMagic@users.noreply.github.com>
2023-11-24 16:09:56 +0530

    upadate new

---
+++ js/slick.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.slider').slick({
+
+    arrows: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+ 
+  });
+
+
+  $('.testi-slider').slick({
+    arrows: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+  })
+
+
+});
